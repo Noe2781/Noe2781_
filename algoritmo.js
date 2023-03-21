6a464e080a5fa95524616ee2e45ed4c50ab74f58
Noe2781 <108550273+Noe2781@users.noreply.github.com>
2023-03-20 22:14:01 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+document.querySelector("body").innerHTML = ` <h1>Bienvenidos a mi pagina</h1>
+<h2>Donde vive el rock</h2>
+<h3>pronto mas novedades</h3>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1s9TWJPyKVvT2ADuveWA-Oz-vO1fGwN9nRiDN_-G2og&s" alt="Purpura">
+
+<img src="imagenes/1969.jpg" alt="1969two">`;
